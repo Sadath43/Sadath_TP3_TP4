9b5bbe8d4f9d29f9ef4eac8a6b24c431364575c0
Sadath43 <sadath.diessongo@centrale-casablanca.ma>
2022-11-20 20:58:45 +0100

    ajout des fichiers

--- dao/game_dao.py
+++ dao/game_dao.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,Column,Integer,String,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship,sessionmaker
 
 engine = create_engine('sqlite:////tmp/tdlog.db', echo=True, future=True)
 Base = declarative_base(bind=engine)
@@ -53,3 +53,82 @@ class WeaponEntity(Base):
     type = Column(String)
     Vessel_id = Column(Integer, ForeignKey("Vessel.id"), nullable=False)
     Vessel= relationship("VesselEntity",back_populates="weapans",uselist=False, cascade="all, delete-orphan")
+
+class GameDao:
+    def __init__(self):
+        Base.metadata.create_all()
+        self.db_session = Session()
+    def create_game(self, game: Game) -> int:
+        game_entity = map_to_game_entity(game)
+        self.db_session.add(game_entity)
+        self.db_session.commit()
+        return game_entity.id
+    def find_game(self, game_id: int) -> Game:
+        stmt = select(GameEntity).where(GameEntity.id == game_id)
+        game_entity = self.db_session.scalars(stmt).one()
+        return map_to_game(game_entity)
+
+    def map_to_game_entity(game: Game) -> GameEntity:
+        game_entity = GameEntity()
+        if game.get_id() is not None:
+            game_entity.id = game.get_id()
+        for player in game.get_players():
+            player_entity = PlayerEntity()
+            player_entity.id = player.id
+            player_entity.name = player.get_name()
+            battlefield_entity = map_to_battlefield_entity(
+                player.get_battlefield())
+            vessel_entities = \
+                map_to_vessel_entities(player.get_battlefield().id,
+                                       player.get_battlefield().vessels)
+            battlefield_entity.vessels = vessel_entities
+            player_entity.battle_field = battlefield_entity
+            game_entity.players.append(player_entity)
+        return game_entity
+
+    def map_to_vessel_entities(battlefield_id: int,vessels:list[Vessel]) \
+            -> list[VesselEntity]:
+        vessel_entities: list[VesselEntity] = []
+        for vessel in vessels:
+            vessel_entity = map_to_vessel_entity(battlefield_id, vessel)
+            vessel_entities.append(vessel_entity)
+
+        return vessel_entities
+
+    def map_to_vessel_entity(battlefield_id: int, vessel: Vessel) -> VesselEntity:
+        vessel_entity = VesselEntity()
+        weapon_entity = WeaponEntity()
+        weapon_entity.id = vessel.weapon.id
+        weapon_entity.ammunitions = vessel.weapon.ammunitions
+        weapon_entity.range = vessel.weapon.range
+        weapon_entity.type = type(vessel.weapon).__name__
+        vessel_entity.id = vessel.id
+        vessel_entity.weapon = weapon_entity
+        vessel_entity.type = type(vessel).__name__
+        vessel_entity.hits_to_be_destroyed = vessel.hits_to_be_destroyed
+        vessel_entity.coord_x = vessel.coordinates[0]
+        vessel_entity.coord_y = vessel.coordinates[1]
+        vessel_entity.coord_z = vessel.coordinates[2]
+        vessel_entity.battle_field_id = battlefield_id
+        return vessel_entity
+
+    def map_to_player_entity(player: Player) -> PlayerEntity:
+        player_entity = PlayerEntity()
+        player_entity.id = player.id
+        player_entity.name = player.name
+        player_entity.battle_field = map_to_battlefield_entity(
+            player.get_battlefield())
+        return player_entity
+
+    def map_to_battlefield_entity(battlefield: Battlefield) -> BattlefieldEntity:
+        battlefield_entity = BattlefieldEntity()
+        battlefield_entity.id = battlefield.id
+        battlefield_entity.max_x = battlefield.max_x
+        battlefield_entity.max_y = battlefield.max_y
+        battlefield_entity.max_z = battlefield.max_z
+        battlefield_entity.min_x = battlefield.min_x
+        battlefield_entity.min_y = battlefield.min_y
+        battlefield_entity.min_z = battlefield.min_z
+        battlefield_entity.max_power = battlefield.max_power
+        return battlefield_entity
+
